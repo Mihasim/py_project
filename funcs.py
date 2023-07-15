3afe1cbf260141cc7b4add8b3573fb2ed73afee1
Mihail Dorozhkin <misha.dorozhkin@mail.ru>
2023-07-15 21:53:34 +0300

    add subtraction

--- funcs.py
+++ funcs.py
@@ -1,7 +1,13 @@
 a = 2
 b = 2
 
-def get_summ(a,b):
+def get_summ(a, b):
+    return a + b
+
+
+def subtraction(a, b):
     return a - b
 
-print(get_summ(a,b))
+
+print(get_summ(a, b))
+print(subtraction(a, b))
